2b0fa7d19cf4cab4f836cd58fdf293e93dbba106
Daniil Gentili <daniil@daniil.it>
2022-04-28 12:50:01 +0200

    Fix

--- src/Psalm/Node/Expr/Cast/VirtualDouble.php
+++ src/Psalm/Node/Expr/Cast/VirtualDouble.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Psalm\Node\Expr\Cast;
 
-use PhpParser\Node\Expr\Cast\Double;
+use PhpParser\Node\Expr\Cast\Double_;
 use Psalm\Node\VirtualNode;
 
-class VirtualDouble extends Double implements VirtualNode
+class VirtualDouble extends Double_ implements VirtualNode
 {
 
 }
