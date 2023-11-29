32b2e493045b5b043ef8cf3a542c6cb538bbcf88
Daniil Gentili <daniil@daniil.it>
2023-11-29 12:44:54 +0100

    Always force JIT

--- src/Psalm/Internal/Cli/Psalm.php
+++ src/Psalm/Internal/Cli/Psalm.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Psalm\Internal\Cli;
 
+use AssertionError;
 use Composer\Autoload\ClassLoader;
 use Psalm\Config;
 use Psalm\Config\Creator;
@@ -903,9 +904,7 @@ final class Psalm
                 . 'Install the opcache extension to make use of JIT for a 20%+ performance boost!'
                 . PHP_EOL . PHP_EOL);
         } elseif (!opcache_get_status(false)['jit']['on']) {
-            $progress->write(PHP_EOL
-                . 'The opcache extension could not be enabled, please report this issue to the Psalm repo!'
-                . PHP_EOL . PHP_EOL);
+            throw new AssertionError('The opcache extension could not be enabled!');
         }
     }
 
